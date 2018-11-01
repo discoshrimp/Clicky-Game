050693b6d58d78a4b4c99b1d347f913338d7bf00
discoshrimp <roydega@gmail.com>
2018-10-31 19:49:23 -0700

    asd

--- card-guess/src/App.js
+++ card-guess/src/App.js
@@ -1,26 +1,56 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import tiles from './components/icons.json'
+import TopBar from './components/TopBar'
+import Wrapper from './components/Wrapper'
+import Tile from './components/Tile'
+
+
 
 class App extends Component {
+
+  initialState = () => {
+    return {
+      tiles: tiles,
+      clicked: [],
+      score: 0,
+    }
+  }
+
+  // shuffle=()=>{
+  //   let randomNumber = Math.floor(Math.random()  * 17)
+  //   return randomNumber
+  // }
+
+  state = {
+    tiles: tiles,
+    clicked: [],
+    score: 0
+  }
+  handleTileClick = (character) => {
+    this.setState({ clicked: [...this.state.clicked, character], score: this.state.score + 1 })
+
+    for (var i = 0; i < this.state.clicked.length; i++) {
+      if (character === this.state.clicked[i]) {
+        this.setState(this.initialState())
+      }
+      if(this.state.score ===16){
+        alert('Yay, you won!')
+        this.setState(this.initialState())
+      }
+
+    }
+  }
+
   render() {
+    console.log(this.state.clicked, this.state.score)
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <Wrapper>
+        <TopBar>{this.state.score}</TopBar>
+        {this.state.tiles.map(tile => (
+          <Tile handleTileClick={this.handleTileClick} name={tile.name} image={tile.image} id={tile.id} key={tile.id} />
+        ))}
+      </Wrapper>
     );
   }
 }
