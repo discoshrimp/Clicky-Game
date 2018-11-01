ca729ddd65cee43f48c692f29cf4fe77fd2e8506
discoshrimp <roydega@gmail.com>
2018-10-31 20:01:37 -0700

    Updates

---
+++ precache-manifest.5dd20aab664cd5112fd583d23170a4e0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "6acb6692e4ee25941da6",
+    "url": "/clicky-game/static/js/runtime~main.6acb6692.js"
+  },
+  {
+    "revision": "ab709c924ff53f593ce4",
+    "url": "/clicky-game/static/js/main.ab709c92.chunk.js"
+  },
+  {
+    "revision": "f843a591dc3acd9073f0",
+    "url": "/clicky-game/static/js/1.f843a591.chunk.js"
+  },
+  {
+    "revision": "ab709c924ff53f593ce4",
+    "url": "/clicky-game/static/css/main.a60ce443.chunk.css"
+  },
+  {
+    "revision": "b332703dd7770dbd21643c4869743b80",
+    "url": "/clicky-game/index.html"
+  }
+];
